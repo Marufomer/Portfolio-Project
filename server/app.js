c20ec43f7b55325521b2f4bf61e9add107fe7c4b
Marufomer <omermaruf07@gmail.com>
2025-01-26 13:52:40 +0300

    user router added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 const port = 4000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// user routes m
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send('hello')
@@ -11,5 +18,5 @@ app.get('/test', (req, res) => {
     res.send("test hani")
 })
 app.listen(port, () => {
-    console.log("listening")
+    console.log(`listening ${port}`)
 });
