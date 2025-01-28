222d8fba4f3da81264a3c08e05299ca208c82e8c
Marufomer <omermaruf07@gmail.com>
2025-01-28 11:14:13 +0300

    middleware completed

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Authentication invalid" });
+  }
+
+  // spilt Bearer
+  const token = authHeader.split(" ")[1];
+  // console.log(authHeader)
+  try {
+    const { first_name, user_id } = jwt.verify(token, process.env.SECRET);
+    // send data
+    req.user = { first_name, user_id };
+    next();
+  } catch (error) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Authentication invalid" });
+  }
+}
+
+export default authMiddleware;
