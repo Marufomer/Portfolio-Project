6c0d2f6370ec868557dfd6b98b05c4bfc73e4949
Marufomer <omermaruf07@gmail.com>
2025-01-28 11:18:04 +0300

    app completed

--- server/app.js
+++ server/app.js
@@ -1,22 +1,38 @@
 import express from 'express';
+import dotenv from "dotenv";
 import userRouter from './routers/userRouter.js';
+import db from './db/dbconfig.js';
+import  cors from 'cors'
+import questionRouter from './routers/questionRoute.js';
+import answerRouter from './routers/answerRouter.js';
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
+app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 // user routes m
 app.use('/api/user', userRouter)
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
+// questions routes
+app.use('/api/question', questionRouter)
 
-app.get('/test', (req, res) => {
-    res.send("test hani")
-})
-app.listen(port, () => {
-    console.log(`listening ${port}`)
-});
+// answer routes
+app.use('/api/answer', answerRouter);
+
+
+async function start() {
+     try {
+        await db.execute("select 'test'");
+        app.listen(port);
+        console.log("database connection established");
+        console.log(`listening on ${port}`);
+     } catch (error) {
+        console.log(error.message);
+     }
+}
+
+start();
