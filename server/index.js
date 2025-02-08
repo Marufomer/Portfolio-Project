8c6acf00c2ea6e0865c3b3f57ea23abad18ce7d8
Marufomer <omermaruf07@gmail.com>
2025-02-08 20:59:37 +0300

    server updated

--- server/index.js
+++ server/index.js
@@ -64,13 +64,11 @@ app.get("/", async (req, res) => {
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
    )`;
-   const sqlT = `ALTER TABLE questions ADD UNIQUE (question_id);`;
+   const sqlT = `ALTER TABLE user ADD UNIQUE (email);`;
 
   try {
    
    await db.query(sqlT);
-    await db.query(sql3);
-    await db.query(sql4);
 
     res.status(StatusCodes.CREATED).json({ msg: "table created!" });
   } catch (error) {
