45b3add6e8b5d3ec5a5da2e26b80c566313ef7a8
Marufomer <omermaruf07@gmail.com>
2025-02-08 20:52:15 +0300

    server updated

--- server/index.js
+++ server/index.js
@@ -1,32 +1,32 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import userRouter from './routers/userRouter.js';
-import db from './db/dbconfig.js';
-import  cors from 'cors'
+import userRouter from "./routers/userRouter.js";
+import db from "./db/dbconfig.js";
+import cors from "cors";
 import { StatusCodes } from "http-status-codes";
-import questionRouter from './routers/questionRoute.js';
-import answerRouter from './routers/answerRouter.js';
+import questionRouter from "./routers/questionRoute.js";
+import answerRouter from "./routers/answerRouter.js";
 dotenv.config();
 
 const app = express();
 const port = 4000;
 
-app.use(cors())
-app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // user routes m
-app.use('/api/user', userRouter)
+app.use("/api/user", userRouter);
 
 // questions routes
-app.use('/api/question', questionRouter)
+app.use("/api/question", questionRouter);
 
 // answer routes
-app.use('/api/answer', answerRouter);
+app.use("/api/answer", answerRouter);
 
-app.get('/', async (req, res) => {
-   const sql1 = `CREATE TABLE if not exists users(
+app.get("/", async (req, res) => {
+  const sql1 = `CREATE TABLE if not exists users(
         user_id int auto_increment,
         firstName varchar(255) not null,
         lastName varchar(255) not null,
@@ -35,7 +35,7 @@ app.get('/', async (req, res) => {
         
         PRIMARY KEY (user_id)
     )`;
-    const sql2 = `CREATE TABLE IF NOT EXISTS questions (
+  const sql2 = `CREATE TABLE IF NOT EXISTS questions (
     id INT AUTO_INCREMENT,
     user_id INT(11) NOT NULL,
     question_id VARCHAR(255) NOT NULL,
@@ -45,51 +45,49 @@ app.get('/', async (req, res) => {
 
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
-   )`
-    const sql3 = `CREATE TABLE if not exists answers(
-      id int auto_increment,
-      user_id int(11) not null,
-      question_id varchar(255) not null,
-      answer varchar(2000) not null,
+   )`;
+  const sql3 = `CREATE TABLE IF NOT EXISTS answers (
+    id INT AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    question_id VARCHAR(255) NOT NULL,
+    answer VARCHAR(2000) NOT NULL,
 
-      PRIMARY KEY (id),
-      FOREIGN KEY (user_id) REFERENCES users(user_id),
-      FOREIGN KEY (question_id) REFERENCES questions(question_id)
-    )`;
-    const sql4 = `CREATE TABLE if not exists images(
-      id int auto_increment,
-      user_id int(11) not null,
-      image varchar(255) not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
+   )`;
+  const sql4 = `CREATE TABLE IF NOT EXISTS images (
+    id INT AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    image VARCHAR(255) NOT NULL,
 
-      PRIMARY KEY (id),
-      FOREIGN KEY (user_id) REFERENCES users(user_id)
-    )`;
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+   )`;
 
-    try {
-      // await db.query(sql1);
-      await db.query(sql2);
-      // await db.query(sql3);
-      // await db.query(sql4);
+  try {
+   
+    await db.query(sql3);
+    await db.query(sql4);
 
-      res.status(StatusCodes.CREATED).json({ msg: "table created!" });
-    } catch (error) {
-      console.log(error.message);
-          return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ message: "something went wrong, try again later!" });
-    }
-})
-
+    res.status(StatusCodes.CREATED).json({ msg: "table created!" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "something went wrong, try again later!" });
+  }
+});
 
 async function start() {
-     try {
-        await db.execute("select 'test'");
-        app.listen(port);
-        console.log("database connection established");
-        console.log(`listening on ${port}`);
-     } catch (error) {
-        console.log(error.message);
-     }
+  try {
+    await db.execute("select 'test'");
+    app.listen(port);
+    console.log("database connection established");
+    console.log(`listening on ${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 start();
