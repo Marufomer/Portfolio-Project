f76573700784511bf7dc3a663cb868a25854f7be
Marufomer <omermaruf07@gmail.com>
2025-02-08 21:31:23 +0300

    server updated

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "https://portfolio-project-production-fbf9.up.railway.app//api/",
+  baseURL: "https://portfolio-project-production-fbf9.up.railway.app/api/",
 });
 
 export default axiosBase;
